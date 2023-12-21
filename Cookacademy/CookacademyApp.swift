69bc585c8e50702a63fa9c62f562d62a34f18de9
Enzo Luigi Schork <enzoluigischork@gmail.com>
2023-12-21 18:04:33 -0300

    Initial Commit

---
+++ Cookacademy/CookacademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookacademyApp.swift
+//  Cookacademy
+//
+//  Created by Enzo Luigi Schork on 21/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CookacademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
